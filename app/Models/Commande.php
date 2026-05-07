1aa8bcc481a43c7e705d26253b83cc4b0a2597be
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-07 20:28:07 +0200

    Model_Commande

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Commande extends Model
 {
-    //
+    protected $fillable = ['client_id', 'fournisseur_id', 'statut', 'date_commande'];
+
+    protected $casts = [
+        'date_commande' => 'datetime',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class, 'client_id');
+    }
+
+    public function fournisseur()
+    {
+        return $this->belongsTo(Fournisseur::class);
+    }
+
+    public function produits()
+    {
+        return $this->belongsToMany(Produit::class, 'commande_produits')
+                    ->withPivot('quantite')
+                    ->withTimestamps();
+    }
+
+    public function commandeProduits()
+    {
+        return $this->hasMany(CommandeProduit::class);
+    }
 }
