30c5beb1c88be43d5b900a3ce0f529e4e013700f
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-07 20:29:29 +0200

    Model_Fournisseur

--- app/Models/Fournisseur.php
+++ app/Models/Fournisseur.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fournisseur extends Model
 {
-    //
+    protected $fillable = ['user_id', 'nom_entreprise', 'adresse', 'telephone'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
+
+    public function commandes()
+    {
+        return $this->hasMany(Commande::class);
+    }
 }
